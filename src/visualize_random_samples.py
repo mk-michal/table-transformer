9bcd2ac200f79fb2083d6ab7ddeb86cd1108320c
Michal Kucirka <michal.kucirka@creativedock.com>
2022-01-05 23:56:31 +0100

    Added visualization of both table detection and structure recognition

--- src/visualize_random_samples.py
+++ src/visualize_random_samples.py
@@ -10,7 +10,7 @@ import util.misc as utils
 from config import Args
 
 from main import get_transform, get_class_map, get_model, get_args
-from table_datasets import PDFTablesDataset, read_pascal_voc
+from table_datasets import PDFTablesDataset, read_pascal_voc, OOSDataset
 from util.box_ops import box_cxcywh_to_xyxy_np
 import matplotlib.pyplot as plt
 
@@ -59,7 +59,6 @@ def save_random_img(xml_path: str, output_path: str, data_type: str = 'structure
 
 
 
-# randomly select 20 frames
 def plot_img_with_bboxes(image, bboxes, classes, output_path, class_labels, xyxy = True):
     # read in image
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
@@ -105,7 +104,7 @@ def plot_img_with_bboxes(image, bboxes, classes, output_path, class_labels, xyxy
             text_coord += 30
             plt.text(20, text_coord, cls, color=class_labels_info[cls]['color'], fontsize=20)
 
-    plt.savefig(output_path)  ## show the plot
+    plt.savefig(output_path, dpi=300)  ## show the plot
 
 
 def main(cfg):
@@ -118,19 +117,20 @@ def main(cfg):
     for key, val in cfg.__dict__.items():
         setattr(args, key, val)
 
-    class_map = get_class_map('structure')
+    class_map = get_class_map(args.data_type)
     model, criterion, postprocessors = get_model(args, args.device)
 
-
-
-    dataset_test = PDFTablesDataset(args.input_path,
-                                    get_transform('structure', "val"),
-                                    do_crop=False,
-                                    make_coco=True,
-                                    image_extension=".jpg",
-                                    xml_fileset="test_filelist.txt",
-                                    class_map=class_map,
-                                    n_of_samples=100)
+    if args.custom:
+        dataset_test = OOSDataset(args.input_path,get_transform(args.data_type, "val"))
+    else:
+        dataset_test = PDFTablesDataset(args.input_path,
+                                        get_transform(args.data_type, "val"),
+                                        do_crop=False,
+                                        make_coco=True,
+                                        image_extension=".jpg",
+                                        xml_fileset="test_filelist.txt",
+                                        class_map=class_map,
+                                        n_of_samples=args.max_samples)
 
     data_loader_test = DataLoader(dataset_test,
                                   2,
@@ -146,27 +146,27 @@ def main(cfg):
         outputs = model(samples)
         orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
         results = postprocessors['bbox'](outputs, orig_target_sizes)
-        plot_results(samples, targets, outputs, results, dataset_test, args.output_path)
+        plot_results(samples, targets, outputs, results, dataset_test, args.output_path, plot_gt=False)
 
 @torch.no_grad()
-def plot_results(samples, targets, outputs, results, dataset, output_path, threshold = 0.5):
-    for mask, sample, target, result, output in zip(samples.mask, samples.tensors, targets, results,
-                                                    outputs['pred_boxes']):
+def plot_results(samples, targets, outputs, results, dataset, output_path, threshold = 0.5, plot_gt: bool = True):
+    for mask, sample, target, result, output in zip(samples.mask, samples.tensors, targets, results, outputs['pred_boxes']):
         s = np.array(sample)
         m = np.array(mask)
         s = s[:, ~m.T[0], :]
         s = s[:, :, ~m[0]]
 
         img_name = dataset.page_ids[target['image_id'].item()]
-        plot_img_with_bboxes(
-            s,
-            bboxes=np.array(target['boxes']),
-            classes=np.array(target['labels']),
-            class_labels=['table', 'table column', 'table row', 'table column header',
-                          'table projected row header', 'table spanning cell', 'no object'],
-            output_path=os.path.join(output_path, f'{img_name}_gt'),
-            xyxy=False,
-        )
+        if plot_gt:
+            plot_img_with_bboxes(
+                s,
+                bboxes=np.array(target['boxes']),
+                classes=np.array(target['labels']),
+                class_labels=['table', 'table column', 'table row', 'table column header',
+                              'table projected row header', 'table spanning cell', 'no object'],
+                output_path=os.path.join(output_path, f'{img_name}_gt'),
+                xyxy=False,
+            )
         threshold = 0.6
 
         bboxes_filtered = np.array(output[result['scores'] > threshold])
@@ -180,7 +180,7 @@ def plot_results(samples, targets, outputs, results, dataset, output_path, thres
             classes=labels_filtered,
             class_labels=['table', 'table column', 'table row', 'table column header',
                           'table projected row header', 'table spanning cell', 'no object'],
-            output_path=os.path.join(output_path, f'{img_name}_pred'),
+            output_path=os.path.join(output_path, f'{img_name.split(".")[0]}_pred.png'),
             xyxy=False,
         )
 
@@ -190,6 +190,9 @@ if __name__ == '__main__':
     parser.add_argument('--output_path', default='/home/michal/datasets/dataset_1_mil/pubtables1m/visualization/')
     parser.add_argument('--input_path', default='/home/michal/datasets/dataset_1_mil/pubtables1m/PubTables-1M-Image_Table_Structure_PASCAL_VOC/PubTables1M-Structure-PASCAL-VOC/test')
     parser.add_argument('--model_load_path', default='/home/michal/datasets/dataset_1_mil/model.pth')
+    parser.add_argument('--max_samples', default=50)
+    parser.add_argument('--data_type', default='structure')
+    parser.add_argument('--custom', action='store_true', default=False)
 
     cfg = parser.parse_args()
     os.makedirs(cfg.output_path, exist_ok=True)
